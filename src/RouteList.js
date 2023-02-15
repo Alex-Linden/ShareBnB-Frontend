d90d1f7794079d82f33e1853c2bff24e02d3b266
Alex Linden <alextl2004@gmail.com>
2023-02-14 16:55:27 -0800

    WIP login/logout routes

--- src/RouteList.js
+++ src/RouteList.js
@@ -1,8 +1,10 @@
+import React, { useContext } from "react";
 import { Navigate, Route, Routes } from "react-router-dom";
 import Homepage from "./Homepage";
 import LoginForm from "./LoginForm";
-import SignupForm from "./SignupForm"
-import ListingForm from "./ListingForm"
+import SignupForm from "./SignupForm";
+import ListingForm from "./ListingForm";
+import userContext from "./UserContext";
 
 /**Routes for browser
  *
@@ -12,16 +14,26 @@ import ListingForm from "./ListingForm"
  * App --> RouterList --> { Homepage, CompanyList, CompanyDetail, JobList, LoginForm,
  * SignupForm, ProfileForm, Logout}
  */
- function RouteList({ login, signup, create }) {
-    console.log("RouteList")
+function RouteList({ login, signup, create }) {
+  const { currUser } = useContext(userContext);
+  console.log("RouteList");
   return (
-    <Routes>
-      <Route path="/" element={<Homepage />} />
-      <Route path="/login" element={<LoginForm login={login}/>} />
-      <Route path="/signup" element={<SignupForm signup={signup} />} />
-      <Route path="/createlisting" element={<ListingForm create={create} />} />
-      <Route path="*" element={<Navigate to="/" />} />
-    </Routes>
+    <div>
+      {currUser.data === null ? (
+
+        <Routes>
+          <Route path="/" element={<Homepage />} />
+          <Route path="/login" element={<LoginForm login={login} />} />
+          <Route path="/signup" element={<SignupForm signup={signup} />} />
+          <Route path="*" element={<Navigate to="/" />} />
+        </Routes>
+      ) : (
+        <Routes>
+          <Route path="/" element={<Homepage />} />
+          <Route path="/createlisting" element={<ListingForm create={create} />} />
+        </Routes>
+      )}
+    </div>
   );
 }
 
